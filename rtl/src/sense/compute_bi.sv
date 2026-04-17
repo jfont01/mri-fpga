48dc8df9d13d06d83ff11b43501c1a66e1e3483c
jfont <julian.font@mi.unc.edu.ar>
2026-04-17 16:36:22 -0300

    compute_bi rtl finished

--- rtl/src/sense/compute_bi.sv
+++ rtl/src/sense/compute_bi.sv
@@ -34,13 +34,19 @@ module compute_Aij #(
 
     logic signed [NB_B - 1 : 0  ]       p0_re;
     logic signed [NB_B - 1 : 0  ]       p0_im;
+
     logic signed [NB_B - 1 : 0  ]       p1_re;
     logic signed [NB_B - 1 : 0  ]       p1_im;
 
-    logic signed [NB_ACC - 1 : 0]       acc0_full;
-    logic signed [NB_B - 1 : 0  ]       acc0_next;
-    logic signed [NB_ACC - 1 : 0]       acc1_full;
-    logic signed [NB_B - 1 : 0  ]       acc1_next;
+    logic signed [NB_ACC - 1 : 0]       acc0_re_full;
+    logic signed [NB_B - 1 : 0  ]       acc0_re_next;
+    logic signed [NB_ACC - 1 : 0]       acc0_im_full;
+    logic signed [NB_B - 1 : 0  ]       acc0_im_next;
+
+    logic signed [NB_ACC - 1 : 0]       acc1_re_full;
+    logic signed [NB_B - 1 : 0  ]       acc1_re_next;
+    logic signed [NB_ACC - 1 : 0]       acc1_im_full;
+    logic signed [NB_B - 1 : 0  ]       acc1_im_next;
 
 
 
@@ -61,41 +67,77 @@ module compute_Aij #(
             .o_im       (p0_im)
         );
 
-        assign acc01_re_full = A01_re + p01_re;
-        assign acc01_im_full = A01_im + p01_im;
+        assign acc0_re_full = b0_re + p0_re;
+        assign acc0_im_full = b0_im + p0_im;
 
         cast #(
             .NB_IN  (NB_ACC),
             .NBF_IN (NBF_ACC),
-            .NB_OUT (NB_A),
-            .NBF_OUT(NBF_A)
-        ) u_cast_acc01_re (
-            .i_word(acc01_re_full),
-            .o_word(acc01_re_next)
+            .NB_OUT (NB_B),
+            .NBF_OUT(NBF_B)
+        ) u_cast_acc0_re (
+            .i_word(acc0_re_full),
+            .o_word(acc0_re_next)
         );
 
         cast #(
             .NB_IN  (NB_ACC),
             .NBF_IN (NBF_ACC),
-            .NB_OUT (NB_A),
-            .NBF_OUT(NBF_A)
-        ) u_cast_acc01_im (
-            .i_word(acc01_im_full),
-            .o_word(acc01_im_next)
+            .NB_OUT (NB_B),
+            .NBF_OUT(NBF_B)
+        ) u_cast_acc0_im (
+            .i_word(acc0_im_full),
+            .o_word(acc0_im_next)
         );
 
-    //! A10
-        assign A10_re = A01_re;
-        assign A10_im = -A01_im;
+    //! p1
+        cmul #(
+            .NB0_IN     (NB_S),
+            .NBF0_IN    (NBF_S),
+            .NB1_IN     (NB_Y),
+            .NBF1_IN    (NBF_Y),
+            .NB_OUT     (NB_B),
+            .NBF_OUT    (NBF_B)
+        ) u_cmul_p0 (
+            .i_re_0     (s1_re[l_ptr_r]),
+            .i_im_0     (-s1_im[l_ptr_r]),
+            .i_re_1     (y_re[l_ptr_r]),
+            .i_im_1     (y_im[l_ptr_r]),
+            .o_re       (p1_re),
+            .o_im       (p1_im)
+        );
+
+        assign acc1_re_full = b1_re + p1_re;
+        assign acc1_im_full = b1_im + p1_im;
+
+        cast #(
+            .NB_IN  (NB_ACC),
+            .NBF_IN (NBF_ACC),
+            .NB_OUT (NB_B),
+            .NBF_OUT(NBF_B)
+        ) u_cast_acc1_re (
+            .i_word(acc1_re_full),
+            .o_word(acc1_re_next)
+        );
+
+        cast #(
+            .NB_IN  (NB_ACC),
+            .NBF_IN (NBF_ACC),
+            .NB_OUT (NB_B),
+            .NBF_OUT(NBF_B)
+        ) u_cast_acc1_im (
+            .i_word(acc1_im_full),
+            .o_word(acc1_im_next)
+        );
 
 
     always_ff @(posedge i_clock) begin
         if (i_rst) begin
             l_ptr_r     <= '0;
-            A00_re   <= '0;
-            A11_re   <= '0;
-            A01_re   <= '0;
-            A01_im   <= '0;
+            b0_re   <= '0;
+            b0_im   <= '0;
+            b1_re   <= '0;
+            b1_im   <= '0;
             running_r <= 1'b0;
             o_valid   <= 1'b0;
         end else begin
@@ -103,16 +145,16 @@ module compute_Aij #(
 
             if (i_start) begin
                 l_ptr_r     <= '0;
-                A00_re   <= '0;
-                A11_re   <= '0;
-                A01_re   <= '0;
-                A01_im   <= '0;
+                b0_re   <= '0;
+                b0_im   <= '0;
+                b1_re   <= '0;
+                b1_im   <= '0;
                 running_r <= 1'b1;
             end else if (running_r) begin
-                A00_re <= acc00_next;
-                A11_re <= acc11_next;
-                A01_re <= acc01_re_next;
-                A01_im <= acc01_im_next;
+                b0_re <= acc0_re_next;
+                b0_im <= acc0_im_next;
+                b1_re <= acc1_re_next;
+                b1_im <= acc1_im_next;
 
                 if (l_ptr_r == L-1) begin
                     running_r <= 1'b0;
